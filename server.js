7f4eda09e73473fdd4255843e584563e3d2b2e43
GOLDI-lab-hub <chandwdkarprashant576@gmail.com>
2025-12-12 19:12:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 import express from "express";
 import cors from "cors";
 import pg from "pg";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import dotenv from "dotenv";
 
 dotenv.config();
